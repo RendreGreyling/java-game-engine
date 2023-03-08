add04c6681031c79cea775cd5aff280d441479b3
Rendre <rendre.greyling@gmail.com>
2023-03-08 22:38:55 +0100

    Add basic lighting

--- src/engineTester/MainGame.java
+++ src/engineTester/MainGame.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -23,20 +24,24 @@ public class MainGame {
         Renderer renderer = new Renderer(shader);
 
 
-        RawModel model = OBJLoader.loadObjModel("stall", loader);
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
         //RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
+        Light light = new Light(new Vector3f(0, 20, -5), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
+
+
         while(!Display.isCloseRequested()) {
-            entity.increaseRotation(0, 1, 0);
+            entity.increaseRotation(0, 0.2f, 0);
             camera.move();
             renderer.prepare();
             shader.start();
+            shader.loadLight(light);
             shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
