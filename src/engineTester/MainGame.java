0d9684d09afc17565ac3279500b7c27726c81c1d
Rendre <rendre.greyling@gmail.com>
2023-03-10 13:19:00 +0100

    Implement Specular lighting and optimizations

--- src/engineTester/MainGame.java
+++ src/engineTester/MainGame.java
@@ -6,12 +6,8 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
 import textures.ModelTexture;
 
 public class MainGame {
@@ -20,34 +16,32 @@ public class MainGame {
 
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
 
 
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
-        TexturedModel staticModel = new TexturedModel(model, texture);
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
+        ModelTexture texture = staticModel.getTexture();
+        texture.setShineDamper(10);
+        texture.setReflectivity(1);
 
 
         Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
-        Light light = new Light(new Vector3f(0, 0, 0), new Vector3f(1,1,1));
+        Light light = new Light(new Vector3f(0, 0, -50), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
+        MasterRenderer renderer = new MasterRenderer();
         while(!Display.isCloseRequested()) {
             entity.increaseRotation(0, 1, 0);
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            shader.stop();
+
+            renderer.processEntity(entity);
+
+            renderer.render(light, camera);
             DisplayManager.updateDisplay();
 
         }
-
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUP();
         DisplayManager.closeDisplay();
 
